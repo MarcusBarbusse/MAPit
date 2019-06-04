c50f564219821dfa7975963018c34f924135de75
Alessandro Dorigo <dorigo.alessandro@gmail.com>
2019-06-04 11:20:26 +0200

    dne some tests with courses#index

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,8 +2,4 @@ class CoursesController < ApplicationController
   def index
     @courses = Course.all
   end
-
-  def show
-    @course = Course.find(params[:id])
-  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,11 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :courses
+  resources :courses do 
+    resources :subscriptions do 
+      resources :subscription_words
+    end
+  end
 
   get 'dashboards', to: 'dashboards#account'
 end
