b7c395995529b7e67f0d45c4d99ff1e9279b2c9a
Isabelle Polle <isabelle.polle@gmail.com>
2019-06-04 16:37:16 +0200

    done

--- app/controllers/search_controller.rb
+++ app/controllers/search_controller.rb
@@ -1,7 +1,7 @@
 class SearchController < ApplicationController
   def index
     if params[:q]
-      @results = Unsplash::Photo.search("cats").first(3)
+      # @results = Unsplash::Photo.search("cats").first(3)
       url = "https://pixabay.com/api/?key=#{ENV['PIXABAY_KEY']}&q=#{params[:q]}&image_type=photo"
       response = HTTParty.get(url)
       @image_results = JSON.parse(response.body)['hits'].first(3).map do |photo|
