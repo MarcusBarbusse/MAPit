cf8f99f86524b6803881514fe9b53140641986fe
louis depraetere <louisdepraetere@hotmail.com>
2019-06-04 16:29:38 +0200

    had a wrong association

--- app/models/subscription_word.rb
+++ app/models/subscription_word.rb
@@ -1,6 +1,5 @@
 class SubscriptionWord < ApplicationRecord
   belongs_to :subscription
-  has_many :course_word_ids
 
   validates :subscription_id, presence: true
   validates :course_word_id, presence: true
