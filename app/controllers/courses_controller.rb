17d0e67edf5309abc95213dd8a70a8948e777bf3
Henry Buisseret <henrybuisseret@gmail.com>
2019-06-04 18:42:57 +0200

    overview courses with all courses selected by language category

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -2,8 +2,13 @@ class CoursesController < ApplicationController
     skip_before_action :authenticate_user!, only: :index
 
   def index
-    @courses = Course.all
-    @courses = policy_scope(Course).order(created_at: :desc)
+    @my_courses = current_user.courses
+    if params[:language].present?
+      @courses = policy_scope(Course.where(language: params[:language]).where.not(id: @my_courses.ids))
+    else
+      @courses = Course.all
+      @courses = policy_scope(Course).order(created_at: :desc)
+    end
   end
 
   def show
