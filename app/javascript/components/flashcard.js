6b2f7131371ef65e49aee87132c46d36db316a96
louis depraetere <louisdepraetere@hotmail.com>
2019-06-05 19:03:08 +0200

    made the original text invisible

--- app/javascript/components/flashcard.js
+++ app/javascript/components/flashcard.js
@@ -1,14 +1,15 @@
-const card = document.getElementById("flashcard");
+const card = document.getElementById("flash");
 const context = document.getElementById("flashcard-context");
 
 const flashcard = () => {
   console.log("hello");
-}
 
 card.addEventListener("click", (event) => {
-    context.innerHTML = ""
-  });
+  console.log(context)
 
+  context.classList.add("flipped");
+  });
+}
 
 export { flashcard };
 
