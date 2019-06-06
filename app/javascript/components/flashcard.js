8103e9af1f1171cee5bb98c6828e332cfc05bac2
louis depraetere <louisdepraetere@hotmail.com>
2019-06-06 18:10:28 +0200

    finished the flashcard

--- app/javascript/components/flashcard.js
+++ app/javascript/components/flashcard.js
@@ -2,10 +2,9 @@ const card = document.getElementById("flash");
 const context = document.getElementById("flashcard-front");
 const answer = document.getElementById("flashcard-answer");
 const flipped = document.getElementById("flashcard");
+const flashed = document.getElementById("button-flashed");
 
 const flashcard = () => {
-  console.log("hello");
-
 card.addEventListener("click", (event) => {
   if (context.classList.contains("invisible")) {
     context.classList.remove("invisible");
