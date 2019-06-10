1d35e9b989f922370769de57b28864d49d62dfaf
Henry Buisseret <henrybuisseret@gmail.com>
2019-06-10 11:53:30 +0200

    seed

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_06_07_134255) do
+ActiveRecord::Schema.define(version: 2019_06_10_093459) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -39,6 +39,7 @@ ActiveRecord::Schema.define(version: 2019_06_07_134255) do
     t.string "language"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "picture"
   end
 
   create_table "subscription_words", force: :cascade do |t|
