5a889644efea71a0b41c1c4f13751c75433cde7e
louis depraetere <louisdepraetere@hotmail.com>
2019-06-10 18:31:44 +0200

    try to get the slide action

--- app/javascript/components/flashcard.js
+++ app/javascript/components/flashcard.js
@@ -5,38 +5,56 @@ const context = document.getElementById("flashcard-front");
 const answer = document.getElementById("flashcard-answer");
 const flipped = document.getElementById("flashcard");
 const flashed = document.getElementById("button-flashed");
+const map = document.getElementById("map")
+// const flashcard = () => {
+//   if (card) {
+//     card.addEventListener("click", (event) => {
+//       if (context.classList.contains("invisible")) {
+//         context.classList.remove("invisible");
+//         answer.classList.add("invisible");
+//         flipped.classList.remove("flipped");
+//         flipped.classList.add("unflipped");
+//       } else {
+//         context.classList.add("invisible");
+//         answer.classList.remove("invisible");
+//         flipped.classList.add("flipped");
+//         // flipped.classList.remove("unflipped");
+//       }
+//       userPictures()
+//     });
+//   }
+// }
 
 const flashcard = () => {
   if (card) {
     card.addEventListener("click", (event) => {
-      if (context.classList.contains("invisible")) {
-        context.classList.remove("invisible");
-        answer.classList.add("invisible");
-        flipped.classList.remove("flipped");
-        flipped.classList.add("unflipped");
-      } else {
-        context.classList.add("invisible");
-        answer.classList.remove("invisible");
-        flipped.classList.add("flipped");
-        // flipped.classList.remove("unflipped");
-      }
+      moving_Image()
+
     });
   }
 }
 
+function moving_Image() {
+  let imageOffset = 0
+  imageOffset += 1000
+  context.style.left = imageOffset + "px";
+}
 export { flashcard };
 
+const userPictures = () => {
+  const photoMother = document.querySelector(".mother_photo_display");
+  const photoTarget = document.querySelector(".target_photo_display");
 
-const photoMother = document.querySelector(".mother_photo_display");
-const MotherId = photoMother.id;
-const locationMother = document.getElementById(MotherId);
+  if (photoMother !== null || photoTarget !== null) {
 
-locationMother.appendChild(photoMother);
+    const MotherId = photoMother.id;
+    const locationMother = document.getElementById(MotherId);
+    const targetId = photoTarget.id;
+    const locationTarget = document.getElementById(targetId);
+    locationMother.appendChild(photoMother);
+    locationTarget.appendChild(photoTarget);
+  };
+};
 
-const photoTarget = document.querySelector(".target_photo_display");
-const targetId = photoTarget.id;
-const locationTarget = document.getElementById(targetId);
-
-locationTarget.appendChild(photoTarget);
-
+userPictures();
 
