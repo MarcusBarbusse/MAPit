91bb8c31db36c997789663739e71c07b80350c46
Isabelle Polle <isabelle.polle@gmail.com>
2019-06-10 15:14:37 +0200

    popper works almost

--- app/javascript/components/flashcard.js
+++ app/javascript/components/flashcard.js
@@ -5,19 +5,21 @@ const flipped = document.getElementById("flashcard");
 const flashed = document.getElementById("button-flashed");
 
 const flashcard = () => {
-card.addEventListener("click", (event) => {
-  if (context.classList.contains("invisible")) {
-    context.classList.remove("invisible");
-    answer.classList.add("invisible");
-    flipped.classList.remove("flipped");
-    flipped.classList.add("unflipped");
-  } else {
-    context.classList.add("invisible");
-    answer.classList.remove("invisible");
-    flipped.classList.add("flipped");
-    // flipped.classList.remove("unflipped");
+  if (card) {
+    card.addEventListener("click", (event) => {
+      if (context.classList.contains("invisible")) {
+        context.classList.remove("invisible");
+        answer.classList.add("invisible");
+        flipped.classList.remove("flipped");
+        flipped.classList.add("unflipped");
+      } else {
+        context.classList.add("invisible");
+        answer.classList.remove("invisible");
+        flipped.classList.add("flipped");
+        // flipped.classList.remove("unflipped");
+      }
+    });
   }
-  });
 }
 
 export { flashcard };
