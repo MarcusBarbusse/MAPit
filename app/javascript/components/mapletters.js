3020387665acc95581bdd1abe4ad35d883c40f0b
Alessandro Dorigo <dorigo.alessandro@gmail.com>
2019-06-12 19:35:15 +0200

    the buttons are now on the right div, the next is outside the loop

--- app/javascript/components/mapletters.js
+++ app/javascript/components/mapletters.js
@@ -1,10 +1,13 @@
 function clickLetter() {
 
-const letters = document.querySelectorAll('.letter');
+  const letters = document.querySelectorAll('.letter');
   const backgroundImageForm = document.getElementById('subscription_word_background_image_id')
   if (letters && backgroundImageForm) {
     document.getElementById(`map 0`).classList.remove('d-none');
     document.getElementById(`form 0`).classList.remove('d-none');
+    document.getElementById(`button1 0`).classList.remove('d-none');
+    document.getElementById(`button2 0`).classList.remove('d-none');      
+    
     const userId = parseInt(backgroundImageForm.value, 10);
     console.log(letters);
     letters.forEach((letter) => {
@@ -16,6 +19,8 @@ const letters = document.querySelectorAll('.letter');
      });
       document.getElementById(`form ${letter.id}`).classList.remove('d-none');
       document.getElementById(`map ${letter.id}`).classList.remove('d-none');
+      document.getElementById(`button1 ${letter.id}`).classList.remove('d-none');
+      document.getElementById(`button2 ${letter.id}`).classList.remove('d-none');
       backgroundImageForm.value = (userId * 26) - 26 + parseInt(letter.id, 10) + 1
     });
    });
