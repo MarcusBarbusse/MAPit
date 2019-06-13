635afde815a04c684f9fffeaff6825e0f5eee6ba
Alessandro Dorigo <dorigo.alessandro@gmail.com>
2019-06-13 18:26:31 +0200

    changed the id

--- app/javascript/components/mapletters.js
+++ app/javascript/components/mapletters.js
@@ -3,8 +3,8 @@ function clickLetter() {
   const letters = document.querySelectorAll('.letter');
   const backgroundImageForm = document.getElementById('subscription_word_background_image_id')
   if (letters && backgroundImageForm) {
-    document.getElementById(`map 0`).classList.remove('d-none');
-    document.getElementById(`form 0`).classList.remove('d-none');
+    document.getElementById(`map-0`).classList.remove('d-none');
+    document.getElementById(`form-0`).classList.remove('d-none');
     // document.getElementById(`form 0`).classList.add('d-flex');
     const userId = parseInt(backgroundImageForm.value, 10);
     console.log(letters);
@@ -14,8 +14,8 @@ function clickLetter() {
          el.classList.add('d-none')
          // el.classList.remove('d-flex')
        });
-        document.getElementById(`form ${letter.id}`).classList.remove('d-none');
-        document.getElementById(`map ${letter.id}`).classList.remove('d-none');
+        document.getElementById(`form-${letter.id}`).classList.remove('d-none');
+        document.getElementById(`map-${letter.id}`).classList.remove('d-none');
         // document.getElementById(`form ${letter.id}`).classList.add('d-flex');
         backgroundImageForm.value = (userId * 26) - 26 + parseInt(letter.id, 10) + 1
       });
