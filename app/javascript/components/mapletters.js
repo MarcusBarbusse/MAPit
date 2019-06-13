c64e6ada109c18b06860dd2bf3b8ca6f005272b1
louis depraetere <louisdepraetere@hotmail.com>
2019-06-13 14:31:59 +0200

    made congratulations page + some fixes on mapping page

--- app/javascript/components/mapletters.js
+++ app/javascript/components/mapletters.js
@@ -5,15 +5,18 @@ function clickLetter() {
   if (letters && backgroundImageForm) {
     document.getElementById(`map 0`).classList.remove('d-none');
     document.getElementById(`form 0`).classList.remove('d-none');
+    // document.getElementById(`form 0`).classList.add('d-flex');
     const userId = parseInt(backgroundImageForm.value, 10);
     console.log(letters);
     letters.forEach((letter) => {
       document.getElementById(letter.id).addEventListener('click', () => {
         document.querySelectorAll(`.maps`).forEach(el => {
          el.classList.add('d-none')
+         // el.classList.remove('d-flex')
        });
         document.getElementById(`form ${letter.id}`).classList.remove('d-none');
         document.getElementById(`map ${letter.id}`).classList.remove('d-none');
+        // document.getElementById(`form ${letter.id}`).classList.add('d-flex');
         backgroundImageForm.value = (userId * 26) - 26 + parseInt(letter.id, 10) + 1
       });
     });
